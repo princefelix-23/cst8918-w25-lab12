77a1fccdb45dde3cd95b275fcebb4304110881ce
PrinceAFelix <princefelix23@icloud.com>
2025-03-31 21:33:12 -0400

    Fix tflint warnings

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -1,11 +1,11 @@
 resource "azurerm_resource_group" "cst8918_rg" {
-  name     = "feli0041-a12-rg"
+  name     = "${var.label_prefix}-a12-rg"
   location = "canadacentral"
 }
 
 # Define the virtual network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.labelPrefix}-A12Vnet"
+  name                = "${var.label_prefix}-A12Vnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.cst8918_rg.location
   resource_group_name = azurerm_resource_group.cst8918_rg.name
@@ -14,7 +14,7 @@ resource "azurerm_virtual_network" "vnet" {
 
 # Define the subnet
 resource "azurerm_subnet" "webserver" {
-  name                 = "${var.labelPrefix}-A12Subnet"
+  name                 = "${var.label_prefix}-A12Subnet"
   resource_group_name  = azurerm_resource_group.cst8918_rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
