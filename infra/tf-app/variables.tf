8e9e63d768fd90a256fa9f981c7df3f009adaa61
PrinceAFelix <princefelix23@icloud.com>
2025-03-31 21:10:30 -0400

    7.0 Add virtual network and subnet to Terraform config

---
+++ infra/tf-app/variables.tf
@@ -0,0 +1,11 @@
+# Define config variables
+variable "labelPrefix" {
+  type        = string
+  default     = "vinh0008"
+  description = "Your college username. This will form the beginning of various resource names."
+}
+
+variable "region" {
+  default = "canadacentral"
+}
+
